82c5bddd23584c76660bf2007bdc1040875d63c1
Joe Bologna <joebologna@mac.com>
2025-04-17 18:03:37 -0500

    Fix issues with scaling add OFF_WHITE

--- apps/appversion2.go
+++ apps/appversion2.go
@@ -3,15 +3,14 @@ package apps
 import (
 	"craps/custom"
 	"embed"
-	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 )
 
 func App2(animationFiles embed.FS) *fyne.Container {
-	theme1 := custom.WidgetTheme{LabelBorderColor: color.RGBA{0, 128, 0, 255}, LabelTextColor: color.White}
-	theme2 := custom.WidgetTheme{LabelBorderColor: color.RGBA{128, 0, 0, 255}, LabelTextColor: color.White}
+	theme1 := custom.WidgetTheme{LabelBorderColor: custom.GREEN, LabelTextColor: custom.OFF_WHITE}
+	theme2 := custom.WidgetTheme{LabelBorderColor: custom.RED, LabelTextColor: custom.OFF_WHITE, Scale: 0.75}
 	l1 := custom.NewLabelWidget("This is a label widget", theme1, false)
 	l2 := custom.NewLabelWidget("This is another label widget", theme1, true)
 	l3 := custom.NewLabelWidget("This is a label widget", theme2, false)
