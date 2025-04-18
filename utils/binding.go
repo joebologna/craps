37d40b21e482ffdd7f5ee0086e7e97040cd11253
Joe Bologna <joebologna@mac.com>
2025-04-17 21:12:44 -0500

    Update app to use custom package, but the button border does not work on iOS

--- utils/binding.go
+++ utils/binding.go
@@ -6,4 +6,10 @@ type BS struct{ binding.String }
 
 func NewBS() BS { return BS{binding.NewString()} }
 
+func NewBSWithString(text string) BS {
+	bs := BS{binding.NewString()}
+	bs.Set(text)
+	return bs
+}
+
 func (s BS) GetS() string { t, _ := s.Get(); return t }
